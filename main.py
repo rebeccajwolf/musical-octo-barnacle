413440c5b55e9c719036be53b850211ef69b3cad
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2025-01-01 06:33:37 +0530

    Update

--- main.py
+++ main.py
@@ -50,6 +50,24 @@ def downloadWebDriver():
         zip_ref.extractall() # you can specify the destination folder path here
     # delete the zip file downloaded above
     os.remove(latest_driver_zip)
+    
+    
+def downloadWebDriverv2():
+    # get the latest chrome driver version number
+    url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
+    response = requests.get(url)
+    version_number = response.text
+
+    # build the donwload url
+    download_url = "https://chromedriver.storage.googleapis.com/" + version_number +"/chromedriver_linux64.zip"
+    # download the zip file using the url built above
+    latest_driver_zip = wget.download(download_url,'chromedriver.zip')
+
+    # extract the zip file
+    with zipfile.ZipFile(latest_driver_zip, 'r') as zip_ref:
+        zip_ref.extractall() # you can specify the destination folder path here
+    # delete the zip file downloaded above
+    os.remove(latest_driver_zip)
 
 
 def main():
@@ -395,6 +413,7 @@ def job():
 
 if __name__ == "__main__":
     # downloadWebDriver()
+    downloadWebDriverv2()
     createDisplay()
     job()
     schedule.every().days.at(time_str = "05:00", tz = "America/New_York").do(job)
