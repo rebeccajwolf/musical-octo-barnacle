dec7177802a4a208eff0e1563a7a132e8835bd4d
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2025-01-07 06:07:22 +0530

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ git pull > /dev/null
 # env >> /etc/environment
 # Xvfb :99 -screen 0 1280x1024x24 -nolisten tcp -nolisten unix &
 # nohup gunicorn keep_alive:app --bind 0.0.0.0:7860 &
-nohup flask --app keep_alive run --host 0.0.0.0 --port 7860 &
+# nohup flask --app keep_alive run --host 0.0.0.0 --port 7860 &
+nohup uvicorn keep_alive_v2:app --host 0.0.0.0 --port 7860 &
 # execute CMD
 bash mkconf.sh &&
 python3 main.py -v -l en -g US
