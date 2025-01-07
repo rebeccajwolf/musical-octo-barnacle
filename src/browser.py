0d020edab9aa90ce643eced4141dfc21f0dd8dd1
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2025-01-07 08:07:16 +0530

    Update

--- src/browser.py
+++ src/browser.py
@@ -133,6 +133,7 @@ class Browser:
                 seleniumwire_options=seleniumwireOptions,
                 user_data_dir=self.userDataDir.as_posix(),
                 # driver_executable_path=getProjectRoot() / "chromedriver",
+                version_main=112,
             )
 
         seleniumLogger = logging.getLogger("seleniumwire")
