42eac678fd1ac68b9c2ad8a441edc23abe584092
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2025-01-09 17:33:09 +0530

    Update

--- run_daily.sh
+++ run_daily.sh
@@ -3,4 +3,4 @@ git pull > /dev/null
 # nohup uvicorn keep_alive:app --host 0.0.0.0 --port 7860 &
 # execute CMD
 bash mkconf.sh &&
-python3 main.py -v -l en -g US
+/app/.venv/bin/python3 main.py -v -l en -g US
