0f7a191b1aa0730227a7982616b1a2c8e1a98481
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2025-01-15 09:21:24 -0500

    Update

--- main.py
+++ main.py
@@ -519,7 +519,7 @@ if __name__ == "__main__":
     )
     
     interface_thread = Thread(target=lambda: iface.launch(
-        server_name="127.0.0.1",
+        server_name="0.0.0.0",
         server_port=7860,
         prevent_thread_lock=True,
         show_api=False,
